1f354bc14bfc5d291447805135931960c679b4eb
colin.kirkham@precisely.com <colin.kirkham@precisely.com>
2023-03-02 11:21:22 +0000

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4ODA3MA/spectral.js?branch_slug=main&token=d86d3f29-2ec3-44a1-b9cc-df7220cc2ea3";
+export default { extends: ruleset };
