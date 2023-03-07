3c92815494c315ffe3aeec5a3cee283908288729
codescrawl <codescrawl@gmail.com>
2023-03-07 09:53:19 +0000

    Update TemplatesApplication.java
    Fixed whitespace.

--- src/main/java/com/precisely/engage/templates/TemplatesApplication.java
+++ src/main/java/com/precisely/engage/templates/TemplatesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemplatesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TemplatesApplication.class, args);
 	}
-
 }
